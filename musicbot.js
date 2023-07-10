37028906b2ca3bd4885381f1337895e7e45d10a0
kesav <87174970+kesav5172@users.noreply.github.com>
2023-07-10 14:08:56 +0530

    Update musicbot.js

--- musicbot.js
+++ musicbot.js
@@ -195,4 +195,4 @@ const error_embed = new Discord.MessageEmbed()
 message.lineReply(error_embed);
 }
 });
-client.login('ODc2NzcyMjI3NzM4MTA3OTQ0.YRo7xA.9rA_Mw7QESRsEg1RnEyL8r69_D8');
+client.login('');
